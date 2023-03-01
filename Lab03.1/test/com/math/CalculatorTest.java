7ae25f258fd97189e8b16b1de88a14dd2d181fe6
Jay Rostosky <jay.rostosky@tlgcohort.com>
2023-03-01 13:24:05 -0800

    Added lifecycles methods to CalculatorTest (@Before, etc)

--- Lab03.1/test/com/math/CalculatorTest.java
+++ Lab03.1/test/com/math/CalculatorTest.java
@@ -9,15 +9,41 @@
 package com.math;
 
 import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class CalculatorTest {
+    // object(s) under test - called a "fixture" in JUnit
+    private Calculator calc;
+
+    @BeforeClass
+    public static void initializeEntireTestRun() {
+        System.out.println("initializeEntireTestRun");
+    }
+
+    @AfterClass
+    public static void finalizeTestRun() {
+        System.out.println("finalizeTestRun");
+    }
+
+    @Before
+    public void setUp() {
+        System.out.println("setUp");
+
+        calc = new Calculator();
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("tearDown");
+    }
 
     @Test
     public void testIsEven() {
         System.out.println("testIsEven");
-
-        Calculator calc = new Calculator();
 
         assertTrue(calc.isEven(10));
         assertFalse(calc.isEven(11));
@@ -27,16 +53,12 @@ public class CalculatorTest {
     public void testDivide() {
         System.out.println("testDivide");
 
-        Calculator calc = new Calculator();
-
         assertEquals(2.5, calc.divide(5, 2), .001);  // expected, actual, delta
     }
 
     @Test
     public void testAdd() {
         System.out.println("testAdd");
-
-        Calculator calc = new Calculator();
 
         assertEquals(5, calc.add(1, 4));             // expected, actual
     }
