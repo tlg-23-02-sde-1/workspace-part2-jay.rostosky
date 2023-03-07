ecc6059fe50659ffbfc9192ccf436d6e240c97b9
Jay Rostosky <jay.rostosky@tlgcohort.com>
2023-03-07 13:52:04 -0800

    Lab 7.2 complete (with Javadocs)

---
+++ Lab07.2/src/com/javatunes/personnel/EmployeeClient.java
@@ -0,0 +1,16 @@
+package com.javatunes.personnel;
+
+class EmployeeClient {
+
+    public static void main(String[] args) {
+        Employee emp = new Employee();
+
+        long begin = System.nanoTime();
+        emp.work();
+        emp.pay();
+        long end = System.nanoTime();
+
+        double elapsedMillis = (end - begin) / 1_000_000.0;
+        System.out.println("Elapsed time (ms): " + elapsedMillis);
+    }
+}
